47d88042bae9d6fac9708af07734b8be9767327d
Srividhya26 <64851602+Srividhya26@users.noreply.github.com>
2021-10-04 10:20:06 +0530

    Front End

---
+++ TimeEntryApp/BusinessObjectLayer/Models/Break.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+#nullable disable
+
+namespace BusinessObjectLayer.Models
+{
+    public partial class Break
+    {
+        public int Id { get; set; }
+        [DataType(DataType.Time)]
+        public TimeSpan? BreakIn { get; set; }
+        [DataType(DataType.Time)]
+        public TimeSpan? BreakOut { get; set; }
+        public int TimeEntryId { get; set; }
+
+        public virtual TimeEntry TimeEntry { get; set; }
+    }
+}
---
+++ TimeEntryApp/BusinessObjectLayer/Models/TimeEntryAppContext.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace BusinessObjectLayer.Models
+{
+    public partial class TimeEntryAppContext : DbContext
+    {
+        public readonly object Entries;
+
+        public TimeEntryAppContext()
+        {
+        }
+
+        public TimeEntryAppContext(DbContextOptions<TimeEntryAppContext> options)
+            : base(options)
+        {
+        }
+    
+        public virtual DbSet<Break> Breaks { get; set; }
+        public virtual DbSet<Role> Roles { get; set; }
+        public virtual DbSet<RoleMapper> RoleMappers { get; set; }
+        public virtual DbSet<TimeEntry> TimeEntries { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server = TRAINEE-04;Database = TimeEntryApp;User id = SA;Password = Srividhya@2000");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+
+            modelBuilder.Entity<Break>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.HasOne(d => d.TimeEntry)
+                    .WithMany(p => p.Breaks)
+                    .HasForeignKey(d => d.TimeEntryId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Breaks__TimeEntr__6383C8BA");
+            });
+
+            modelBuilder.Entity<Role>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.RoleName).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<RoleMapper>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("RoleMapper");
+
+                entity.HasOne(d => d.Role)
+                    .WithMany()
+                    .HasForeignKey(d => d.RoleId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__RoleMappe__RoleI__5DCAEF64");
+
+                entity.HasOne(d => d.User)
+                    .WithMany()
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__RoleMappe__UserI__5CD6CB2B");
+            });
+
+            modelBuilder.Entity<TimeEntry>(entity =>
+            {
+                entity.ToTable("TimeEntry");
+
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Date).HasColumnType("date");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.TimeEntries)
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__TimeEntry__UserI__60A75C0F");
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Address).HasMaxLength(50);
+
+                entity.Property(e => e.Dob)
+                    .HasColumnType("datetime")
+                    .HasColumnName("DOB");
+
+                entity.Property(e => e.Name).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}

1588e4a2be2b491913ba87e317ce4c0ce3f81cfa
Srividhya26 <venksri8@gmail.com>
2021-11-09 13:06:25 +0530

    UI

--- InterviewEvaluation/UI/src/app/app-routing.module.ts
+++ InterviewEvaluation/UI/src/app/app-routing.module.ts
@@ -1,7 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { RegisterComponentComponent } from './authentication/register-component/register-component.component';
+import { ResultComponent } from './hr-dashboard/result/result.component';
+import { AddCandidateComponent } from './hr-dashboard/add-candidate/add-candidate.component';
+import { HrDashboardComponent } from './hr-dashboard/hr-dashboard.component';
+import { ScheduleComponent } from './hr-dashboard/schedule/schedule.component';
+import { InterviewerDashboardComponent } from './interviewer-dashboard/interviewer-dashboard.component';
+import { RatingComponent } from './interviewer-dashboard/rating/rating.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', component : AuthenticationComponent
+  },
+  {
+    path : 'hrdashboard',component : HrDashboardComponent
+  },
+  { 
+    path:'addCandidate',component:AddCandidateComponent
+  },
+  {
+    path:'register',component:RegisterComponentComponent
+  },
+  {
+    path:'schedule',component:ScheduleComponent
+  },
+  {
+    path:'result',component:ResultComponent
+  },
+  {
+    path:'interviewer',component:InterviewerDashboardComponent
+  },
+  {
+    path:'rating',component:RatingComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- InterviewEvaluation/UI/src/app/app.module.ts
+++ InterviewEvaluation/UI/src/app/app.module.ts
@@ -3,10 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HrDashboardComponent } from './hr-dashboard/hr-dashboard.component';
+import { AddCandidateComponent } from './hr-dashboard/add-candidate/add-candidate.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { RegisterComponentComponent } from './authentication/register-component/register-component.component';
+import { ScheduleComponent } from './hr-dashboard/schedule/schedule.component';
+import { ResultComponent } from './hr-dashboard/result/result.component';
+import { InterviewerDashboardComponent } from './interviewer-dashboard/interviewer-dashboard.component';
+import { RatingComponent } from './interviewer-dashboard/rating/rating.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HrDashboardComponent,
+    AddCandidateComponent,
+    AuthenticationComponent,
+    RegisterComponentComponent,
+    ScheduleComponent,
+    ResultComponent,
+    InterviewerDashboardComponent,
+    RatingComponent,
   ],
   imports: [
     BrowserModule,
